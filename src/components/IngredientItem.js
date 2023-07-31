75af8849b1d6ecb97388e7594e3ff0895c15f6aa
Bajda <bajdster@gmail.com>
2023-07-31 21:25:23 +0100

    added fetching ingredients

--- src/components/IngredientItem.js
+++ src/components/IngredientItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import classes from "./IngredientItem.module.scss"
 
-const IngredientItem = ({name, image, id}) => {
+const IngredientItem = ({name, image, id, addIngredient}) => {
   return (
     <div className={classes.ingredientItem}>
-        <p>{name}</p>
+        
         <img src={'https://spoonacular.com/cdn/ingredients_100x100/'+image}></img>
+        <p>{name}</p>
+        <button onClick = {()=>
+        {
+            addIngredient(name)
+        }}>+</button>
     </div>
   )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -6,41 +6,98 @@ import IngredientItem from './IngredientItem'
 const Input = () => {
 
     const [ingredient, setIngredient] = useState("");
-    const [ingredients, setIngredients] = useState([]);
+    const [allIngredients, setAllIngredients] = useState({results:[{name:"milk", id: 1, img: "milk.png"}, {name:"eggs", id: 2, img: "milk.png"}]});
+    const [usedIngredients, setUsedIngredients] = useState([])
+    const [recipeSearchingMode, setRecipeSearchingMode] = useState(false)
 
     const getIngredient = (e) =>
     {   
         setIngredient(e.target.value)
     }
 
+    const clearInputHandler = ()=>
+    {
+        setIngredient("")
+    }
+
+    const addUsedIngredients = (name) =>
+    {
+        setUsedIngredients((prev)=>
+        {
+            return [...prev, name]
+        })
+    }
+
+    const searchRecipes = (e) =>
+    {
+        e.preventDefault();
+        setRecipeSearchingMode(true)
+        let ingredients = "";
+        usedIngredients.forEach((el,index)=>
+            {
+                if(index === 0) ingredients+=el;
+                else ingredients+=`+${el}`
+            })
+        // const searchRecipes = async () =>
+        // {
+        //     const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=12&apiKey=3a9aab141bca4fdaa4ed1028bfbe27b1`)
+        //     const data = await response.json();
+
+        //     console.log(data)
+        // }
+
+
+        // searchRecipes()
+    }
+
     useEffect(()=>
     {
-        const searchIngredients = async () =>
-        {
-            const response = await fetch(`https://api.spoonacular.com/food/ingredients/search?query=${ingredient}&number=12&apiKey=3a9aab141bca4fdaa4ed1028bfbe27b1`)
-            const data = await response.json();
-            setIngredients(data);
-        }
+        setRecipeSearchingMode(false);
+        
+        // const searchIngredients = async () =>
+        // {
+        //     const response = await fetch(`https://api.spoonacular.com/food/ingredients/search?query=${ingredient}&number=12&apiKey=3a9aab141bca4fdaa4ed1028bfbe27b1`)
+        //     const data = await response.json();
+        //     setAllIngredients(data);
+        // }
 
-        searchIngredients()
+       
+        const typingTimer = setTimeout(()=>
+        {
+            // searchIngredients()
+            console.log("fetch recipes")
+        }, 1500)
+
+        return ()=>
+        {
+            clearInterval(typingTimer)
+        }
         
     }, [ingredient])
 
-console.log(ingredients.results)
+console.log(allIngredients.results)
 
     
   return (
     <div className={classes.search}>
     <form>
+        {usedIngredients.length>0 && <button className={classes.searchRecipes} onClick={searchRecipes}>Search</button>}
         <input type="text" placeholder='Search ingredients' onChange={getIngredient} value={ingredient}></input>
-        <FaWindowClose className={classes.close}/>
+        {ingredient !== "" && <FaWindowClose className={classes.close} onClick ={clearInputHandler}/>}
        
     </form>
+    
+    <ul className={classes.usedIngredients}>
+        {usedIngredients.map((item, index)=>
+        {
+            return <li key={index}>{item}</li>
+        })}
+    </ul>
 
     <div className={classes.ingredients}>
-        {ingredients.results && ingredients.results.map(item=>
+        {allIngredients.results && !recipeSearchingMode && allIngredients.results.map(item=>
         {
-            return <IngredientItem name={item.name} image={item.image} id={item.id}/>
+            return <IngredientItem name={item.name} image={item.image} id={item.id} key={item.id} addIngredient={addUsedIngredients}/>
         })}
     </div>
 </div>
